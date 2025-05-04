ec92a0b3f042e375207a5e25cf5315922a198901
eduardo <eduarduhh@live.com>
2025-05-04 16:05:59 -0300

    fix: corrigindo cors

--- src/main/kotlin/br/com/eduarduhh/currencyconverter/controller/TransactionController.kt
+++ src/main/kotlin/br/com/eduarduhh/currencyconverter/controller/TransactionController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/transaction")
+@RequestMapping("/api/v1/transaction")
 class TransactionController(
     private val transactionService: TransactionService
 ) {
